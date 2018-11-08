9ac986d10a6d93e94f37942bf60ba2cae1c903ac
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-08 13:57:45 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ end
 
 def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|call| call.capitalize }
-  planeteer_calls.each { |call| puts name + '!' }
+  planeteer_calls.each { |call| puts call + '!' }
 end
 
 def long_planeteer_calls# code an argument here
