5616dd377fbdc194f3c5dd6516dcbb663d2860ea
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-08 13:56:01 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,9 @@ def roll_call_dwarves(dwarves)
      puts "#{index +1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  names.map! {|call| call.capitalize } 
+  names.each { |call| puts name + '!' }
 end
 
 def long_planeteer_calls# code an argument here
