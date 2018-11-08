d5942ef0745f84fc9f9a99fa6daf3fa797f3da83
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-08 14:59:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|call| call.capitalize + "!" }
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call.length > 4}
 end
 
 def find_the_cheese# code an argument here
